c139f89ae71484bbf29667cea56a81e3a3d2dfbb
Astrid <dawsonastrid@yahoo.es>
2024-04-10 20:57:46 -0500

    practica de los conceptos

---
+++ scripts/practicaConceptos.js
@@ -0,0 +1,6 @@
+// 1. SIEMPRE SE DEBE HACER --> VERIFICAR SI ESTA CONECTADO CON EL HTML|
+
+console.log("Hola soy la actividad de hoy");
+
+const edadInput = document.getElementById ("edadInput");
+console.log(edadInput)
